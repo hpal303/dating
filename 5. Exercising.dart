b3d19ef7570a8501c0ec15386b219fe44c840b38
Harminder Pal Monga <109013863+hpal303@users.noreply.github.com>
2022-07-26 22:44:01 +0530

    Add files via upload

---
+++ 5. Exercising.dart
@@ -0,0 +1,262 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hookup4u/Screens/SexualOrientation.dart';
+import 'package:hookup4u/Screens/UserName.dart';
+import 'package:hookup4u/util/color.dart';
+import 'package:hookup4u/util/snackbar.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+class Exercising extends StatefulWidget {
+  final Map<String, dynamic> userData;
+  Exercising(this.userData);
+
+  @override
+  _ExercisingState createState() => _ExercisingState();
+}
+
+class _ExercisingState extends State<Exercising> {
+  bool socially = false;
+  bool frequently = false;
+  bool never = false;
+  bool select = false;
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      backgroundColor: Colors.white,
+      floatingActionButton: AnimatedOpacity(
+        opacity: 1.0,
+        duration: Duration(milliseconds: 50),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 10.0),
+          child: FloatingActionButton(
+            elevation: 10,
+            child: IconButton(
+              color: secondryColor,
+              icon: Icon(Icons.arrow_back_ios),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            backgroundColor: Colors.white38,
+            onPressed: () {
+              dispose();
+              Navigator.pop(context);
+            },
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+      body: Stack(
+        children: <Widget>[
+          Padding(
+            child: Text(
+              "I exercise".tr().toString(),
+              style: TextStyle(fontSize: 40),
+            ),
+            padding: EdgeInsets.only(left: 50, top: 120),
+          ),
+          Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                OutlineButton(
+                  highlightedBorderColor: primaryColor,
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * .065,
+                    width: MediaQuery.of(context).size.width * .75,
+                    child: Center(
+                        child: Text("SOCIALLY".tr().toString(),
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: socially ? primaryColor : secondryColor,
+                                fontWeight: FontWeight.bold))),
+                  ),
+                  borderSide: BorderSide(
+                      width: 1,
+                      style: BorderStyle.solid,
+                      color: socially ? primaryColor : secondryColor),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25)),
+                  onPressed: () {
+                    setState(() {
+                      frequently = false;
+                      socially = true;
+                      never = false;
+                    });
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: OutlineButton(
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * .065,
+                      width: MediaQuery.of(context).size.width * .75,
+                      child: Center(
+                          child: Text("FREQUENTLY".tr().toString(),
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: frequently ? primaryColor : secondryColor,
+                                  fontWeight: FontWeight.bold))),
+                    ),
+                    borderSide: BorderSide(
+                      color: frequently ? primaryColor : secondryColor,
+                      width: 1,
+                      style: BorderStyle.solid,
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25)),
+                    onPressed: () {
+                      setState(() {
+                        frequently = true;
+                        socially = false;
+                        never = false;
+                      });
+                      // Navigator.push(
+                      //     context, CupertinoPageRoute(builder: (context) => OTP()));
+                    },
+                  ),
+                ),
+                OutlineButton(
+                  focusColor: primaryColor,
+                  highlightedBorderColor: primaryColor,
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * .065,
+                    width: MediaQuery.of(context).size.width * .75,
+                    child: Center(
+                        child: Text("NEVER".tr().toString(),
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: never ? primaryColor : secondryColor,
+                                fontWeight: FontWeight.bold))),
+                  ),
+                  borderSide: BorderSide(
+                      width: 1,
+                      style: BorderStyle.solid,
+                      color: never ? primaryColor : secondryColor),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25)),
+                  onPressed: () {
+                    setState(() {
+                      frequently = false;
+                      socially = false;
+                      never = true;
+                    });
+                    // Navigator.push(
+                    //     context, CupertinoPageRoute(builder: (context) => OTP()));
+                  },
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 100.0, left: 10),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: ListTile(
+                leading: Checkbox(
+                  activeColor: primaryColor,
+                  value: select,
+                  onChanged: (newValue) {
+                    setState(() {
+                      select = newValue!;
+                    });
+                  },
+                ),
+                title: Text("Show on my profile".tr().toString()),
+              ),
+            ),
+          ),
+          socially || frequently || never
+              ? Padding(
+                  padding: const EdgeInsets.only(bottom: 40),
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: InkWell(
+                      child: Container(
+                          decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              borderRadius: BorderRadius.circular(25),
+                              gradient: LinearGradient(
+                                  begin: Alignment.topRight,
+                                  end: Alignment.bottomLeft,
+                                  colors: [
+                                    primaryColor.withOpacity(.5),
+                                    primaryColor.withOpacity(.8),
+                                    primaryColor,
+                                    primaryColor
+                                  ])),
+                          height: MediaQuery.of(context).size.height * .065,
+                          width: MediaQuery.of(context).size.width * .75,
+                          child: Center(
+                              child: Text(
+                            "CONTINUE".tr().toString(),
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: textColor,
+                                fontWeight: FontWeight.bold),
+                          ))),
+                      onTap: () {
+                        var userExercising;
+                        if (socially) {
+                          userExercising = {
+                            'userExercising': "socially",
+                            'showOnProfile': select
+                          };
+                        } else if (frequently) {
+                          userExercising = {
+                            'userExercising': "frequently",
+                            'showOnProfile': select
+                          };
+                        } else {
+                          userExercising = {
+                            'userExercising': "never",
+                            'showOnProfile': select
+                          };
+                        }
+                        widget.userData.addAll(userExercising);
+                        // print(userData['userExercising']['showOnProfile']);
+                        Navigator.push(
+                            context,
+                            CupertinoPageRoute(
+                                builder: (context) =>
+                                    SexualOrientation(widget.userData)));
+                        //      ads.disable(ad1);
+                      },
+                    ),
+                  ),
+                )
+              : Padding(
+                  padding: const EdgeInsets.only(bottom: 40),
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: InkWell(
+                      child: Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          height: MediaQuery.of(context).size.height * .065,
+                          width: MediaQuery.of(context).size.width * .75,
+                          child: Center(
+                              child: Text(
+                            "CONTINUE".tr().toString(),
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: secondryColor,
+                                fontWeight: FontWeight.bold),
+                          ))),
+                      onTap: () {
+                        CustomSnackbar.snackbar(
+                            "Please select one".tr().toString(), _scaffoldKey);
+                      },
+                    ),
+                  ),
+                )
+        ],
+      ),
+    );
+  }
+}
